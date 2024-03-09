4b3dd601a30918d5040d0e6f6cb6d04177e8161d
iamkhushbooy <khushbooygkp@gmail.com>
2024-03-09 17:31:52 +0530

    new project

---
+++ app/components/Tasklist.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Tasklist = ({ date, title, des, del }) => {
+    return (
+        <div className='w-[350px] min-h-[150px] flex justify-around items-center flex-col text-blue-200 mb-10  bg-slate-800 rounded-lg'>
+            <div className='min-w-[250px] flex justify-around items-center text-blue-300'>
+                <p className='font-bold'> Date:</p>
+                <p className='text-[#cccccc]'>{date}</p>
+            </div>
+            <div className="title flex text-blue-300">
+                <p className='w-[50px]  font-bold'>Title:</p>
+                <p className='text-[#cccccc]'>{title}</p>
+            </div>
+           <div className=" w-[100%] min-h-[100px] title flex justify-center items-center text-blue-300 flex-col">
+                <p className='font-bold'>Description:</p>
+                <p className='text-[#cccccc] text-justify px-5 py-2'>{des}</p>
+            </div>
+            <button onClick={del} className='w-[100px] h-[30px] bg-red-400 rounded text-black'>Delete</button>
+
+
+        </div>
+    )
+}
+
+export default Tasklist
---
+++ app/date/[date]/page.jsx
@@ -0,0 +1,46 @@
+'use client'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import React from 'react'
+import Tasklist from '@/app/components/Tasklist'
+import { useParams } from 'next/navigation'
+const page = () => {
+    let params = useParams();
+    let [data, setData] = useState([]);
+    let Show = () => {
+        let d=params.date
+        let formatdate=d.split('-').join('/')
+        let wholedata=JSON.parse(localStorage.getItem('data'))
+        let searchdata=wholedata.filter((item,index)=>(
+            item.d.startsWith(formatdate)
+        ))
+        console.log(searchdata);
+        setData(searchdata)
+    }
+    useEffect(() => {
+        Show()
+    }, [params.date])
+    const del = (i) => {
+        const newarray = data.slice().reverse().filter((item, index) => (
+            i !== index
+        ))
+        localStorage.setItem("data", JSON.stringify(newarray))
+        setData(newarray)
+    }
+
+    return (
+        <>
+            <div className='w-full min-h-screen bg-[rgba(0,0,0,0.9)] flex justify-around items-center flex-col'>
+
+                <div id="tasks">
+                    {data && data.slice().reverse().map((item, index) => (
+                        <Tasklist key={index} date={item.d} task={item.task} title={item.title} des={item.des} del={() => del(index)} />
+                    ))}
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+export default page
